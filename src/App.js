d98a63fd652590069d9be5275327c3563ed49edc
imexotic <hubert.stoklosa23@gmail.com>
2022-11-25 16:00:52 +0000

    Add empty (for now) Hero component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import SocialSidebar from "./components/SocialSidebar";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 import "./styles/App.css";
 
@@ -7,7 +9,9 @@ const App = () => {
    return (
       <div>
          <Header />
+         <Hero />
          <SocialSidebar />
+         <Footer />
       </div>
    );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import "../styles/Hero.css";
+
+const Hero = () => {
+   return (
+      <section>
+         <div className="container"></div>
+      </section>
+   );
+};
+
+export default Hero;
