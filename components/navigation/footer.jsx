f997052f09e9b1256377993cee01bf0295cb74b4
imexotic <hubert.stoklosa23@gmail.com>
2022-12-21 17:03:03 +0000

    modify footer

--- components/navigation/footer.jsx
+++ components/navigation/footer.jsx
@@ -10,22 +10,26 @@ const Footer = () => {
       <footer id={styles["footer"]}>
          <div className={`${styles["container"]} container`}>
             <SocialLinks _styles={styles} />
+            <p className={styles["developer"]}>
+               {/* &copy;&nbsp;
+               <a href="#about-section" className="url-link">
+                  Hubert Stoklosa
+               </a>
+               &nbsp; 2022 — All Rights Reserved */}
+               Developed by{" "}
+               <a href="https://github.com/imexotic" target="__blank" className="url-link">
+                  Hubert Stoklosa
+               </a>
+            </p>
             <p className={styles["footer-text"]}>
                Built with&nbsp;
                <a className="url-link" href="https://nextjs.org/" target="__blank">
                   Next.js
                </a>
-               &nbsp;&nbsp;Powered by&nbsp;
+               &nbsp;&nbsp;—&nbsp;&nbsp;Powered by&nbsp;
                <a className="url-link" href="https://vercel.com/dashboard" target="__blank">
                   Vercel
                </a>
-            </p>
-            <p className={styles["copyright"]}>
-               &copy;&nbsp;
-               <a href="#about-section" className="url-link">
-                  Hubert Stoklosa
-               </a>
-               &nbsp; 2022 — All Rights Reserved
             </p>
          </div>
       </footer>
