4d627eb8cdc687730753cb9f59994add4de5b345
imexotic <hubert.stoklosa23@gmail.com>
2022-12-22 17:34:23 +0000

    modify site's head (favicon & title)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "../styles/styles.css";
 
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-   return <Component {...pageProps} />;
+   return (
+      <div>
+         <Head>
+            <title>Hubert Stoklosa - Student & Developer</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest"></link>
+         </Head>
+         <Component {...pageProps} />
+      </div>
+   );
 }
