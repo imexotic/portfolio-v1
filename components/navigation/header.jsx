3b26071b1da9f354235ee64ebde7591078675ed8
imexotic <hubert.stoklosa23@gmail.com>
2022-12-24 00:53:42 +0000

    modify header on smaller screens

--- components/navigation/header.jsx
+++ components/navigation/header.jsx
@@ -4,30 +4,22 @@ import { useState, useEffect } from "react";
 import { IconContext } from "react-icons";
 import { FiSun, FiMoon, FiMenu } from "react-icons/fi";
 
-import useScrollDirection from "../../hooks/useScrollDirection";
-
-const Header = ({ theme, toggleTheme }) => {
+const Header = ({ scrollPosition, scrollDirection, theme, toggleTheme }) => {
    const [toggle, setToggle] = useState(false); // burger menu toggler
-   const [icon, setIcon] = useState(); // header icon based on current theme
-   const [scrollPosition, scrollDirection] = useScrollDirection();
 
    useEffect(() => {
       toggle ? document.body.classList.add("no-scroll") : document.body.classList.remove("no-scroll");
    }, [toggle]);
 
-   useEffect(() => {
-      setIcon(theme === "dark" ? <FiSun /> : <FiMoon />);
-   }, [theme]);
-
    // check if burger menu is open
    const handleMenuClose = () => toggle && setToggle(false);
 
    return (
-      <header id={styles["header"]} className={`${scrollDirection === "down" ? styles["hide"] : styles["show"]} ${scrollPosition === 0 ? styles["top"] : styles["scroll"]} ${toggle ? styles["show-menu"] : styles["hide-menu"]}`}>
+      <header id={styles["header"]} className={`${scrollDirection === "down" ? styles["hide"] : styles["show"]} ${scrollPosition === 0 ? styles["top"] : styles["scrolling"]} ${toggle ? styles["show-menu"] : styles["hide-menu"]}`}>
          <div className={`${styles["container"]} container`}>
             <h1>Hubert.</h1>
-            <nav className={styles["navbar"]}>
-               <ul className={toggle === true ? styles["show-nav"] : styles["hide-nav"]}>
+            <nav className={styles["navbar"]} onClick={() => setToggle(false)}>
+               <ul onClick={(e) => e.stopPropagation()}>
                   <li className={styles["nav-item"]}>
                      <a href="#about-section" className={styles["nav-link"]} onClick={() => handleMenuClose()}>
                         About
@@ -43,22 +35,27 @@ const Header = ({ theme, toggleTheme }) => {
                         Contact
                      </a>
                   </li>
+                  <li className={styles["nav-item"]}>
+                     <input type="checkbox" className={styles["checkbox"]} id="checkbox" defaultChecked={theme === "light" ? true : false} onChange={() => toggleTheme()} />
+                     <label for="checkbox" className={styles["checkbox-label"]}>
+                        <IconContext.Provider value={{ className: styles["icon"] }}>
+                           <FiSun />
+                        </IconContext.Provider>
+                        <IconContext.Provider value={{ className: styles["icon"] }}>
+                           <FiMoon />
+                        </IconContext.Provider>
+                        <span className={styles["ball"]}></span>
+                     </label>
+                  </li>
                </ul>
             </nav>
-            <div className={styles["header-btns"]}>
-               <button className={styles["header-btn"]} onClick={toggleTheme}>
-                  <IconContext.Provider value={{ className: styles["theme-icon"] }}>
-                     <>{icon}</>
-                  </IconContext.Provider>
-               </button>
-               <button className={(styles["header-btn"], styles["burger-btn"])} onClick={() => setToggle(!toggle)}>
-                  <IconContext.Provider value={{ className: styles["theme-icon"] }}>
-                     <>
-                        <FiMenu />
-                     </>
-                  </IconContext.Provider>
-               </button>
-            </div>
+            <button className={(styles["header-btn"], styles["burger-btn"])} onClick={(e) => setToggle(!toggle)}>
+               <IconContext.Provider value={{ className: styles["icon"] }}>
+                  <>
+                     <FiMenu />
+                  </>
+               </IconContext.Provider>
+            </button>
          </div>
       </header>
    );
